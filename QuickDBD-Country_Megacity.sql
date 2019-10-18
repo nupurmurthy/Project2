d4b25618d05ba567be16bbecbea7374cfcc6c6ac
klhi3 <hekwang.lhi@gmail.com>
2019-10-17 20:02:53 -0400

    Initial commit

--- QuickDBD-Country_Megacity.sql
+++ QuickDBD-Country_Megacity.sql
@@ -32,30 +32,6 @@ CREATE TABLE "megacities_transformed" (
      )
 );
 
-CREATE TABLE "merged" (
-    "country_code" char(2)   NOT NULL,
-    "continent" varchar(20)   NOT NULL,
-    "capital" varchar(30)   NOT NULL,
-    "country_population" integer   NOT NULL,
-    "area" integer   NOT NULL,
-    "coastline" integer   NOT NULL,
-    "government" varchar(90)   NOT NULL,
-    "currency" varchar(40)   NOT NULL,
-    "birthrate" float   NOT NULL,
-    "deathrate" float   NOT NULL,
-    "city_name" char(30)   NOT NULL,
-    "city_population" integer   NOT NULL,
-    "latitude" float   NOT NULL,
-    "longitude" float   NOT NULL,
-    "region" varchar(40)   NOT NULL
-);
-
 ALTER TABLE "megacities_transformed" ADD CONSTRAINT "fk_megacities_transformed_country_code" FOREIGN KEY("country_code")
 REFERENCES "countries_transformed" ("country_code");
-
-ALTER TABLE "merged" ADD CONSTRAINT "fk_merged_country_code" FOREIGN KEY("country_code")
-REFERENCES "countries_transformed" ("country_code");
-
-ALTER TABLE "merged" ADD CONSTRAINT "fk_merged_city_name" FOREIGN KEY("city_name")
-REFERENCES "megacities_transformed" ("city_name");
 
